a87f1d51a1f444af5affb1c21750d836b75a9cac
Dmitriy Bukatsel <dimasgrand16@gmail.com>
2023-03-19 23:07:19 +0300

    fix: correct output for numbers in random

--- src/commands/entertainment/random.js
+++ src/commands/entertainment/random.js
@@ -7,14 +7,17 @@ module.exports = {
         .addIntegerOption(option => option.setName('from').setDescription('От:').setRequired(true))
         .addIntegerOption(option => option.setName('to').setDescription('До:').setRequired(true)),
     async execute(interaction) {
-        const from = interaction.options.getInteger('from');
-        const to = interaction.options.getInteger('to');
+        const from = interaction.options.getInteger('from'),
+            to = interaction.options.getInteger('to');
 
-        const random = Math.floor(Math.random() * (to - from + 1) + from);
+        const max = Math.max(from, to),
+            min = Math.min(from, to);
+
+        const random = Math.floor(Math.random() * (max - min + 1) + min);
 
         const embed = new EmbedBuilder()
             .setColor('#018c08')
-            .setTitle(`Случайное число от ${from} до ${to}:`)
+            .setTitle(`Случайное число от ${min} до ${max}:`)
             .setDescription(`${random}`);
 
         await interaction.reply({ embeds: [embed] });
