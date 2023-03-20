5bc1abae34ac840164df7ee4888a8a999c998358
Dmitriy Bukatsel <dimasgrand16@gmail.com>
2023-03-20 15:11:52 +0300

    feat: remove file, added discord text formatter for json

--- src/commands/utilities/printServerInfo.js
+++ src/commands/utilities/printServerInfo.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
-const fs = require('fs');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 const { servers } = require('../../db');
 const accessDenied = require('../../utils/accessDenied');
@@ -12,30 +11,18 @@ module.exports = {
         if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
             try {
                 await interaction.deferReply({ ephemeral: true });
-                await interaction.editReply({ content: 'Файл генерируется, пожалуйста подождите...' });
 
                 const data = await servers.get(interaction.guild.id);
-
                 if (Object.keys(data).length === 0) throw new Error();
 
-                fs.writeFileSync(`./src/db/${interaction.guild.id}.json`, JSON.stringify(data, null, ' '));
+                await interaction.editReply({ content: 'Файл генерируется, пожалуйста подождите...' });
 
-                const file = new AttachmentBuilder(`./src/db/${interaction.guild.id}.json`);
-
-                const embed = new EmbedBuilder().setTitle('Данные сервера в формате JSON').setColor('#018c08');
-
-                await interaction.editReply({
-                    content: '',
-                    embeds: [embed],
-                    files: [file],
-                });
-
-                fs.unlinkSync(`./src/db/${interaction.guild.id}.json`);
-            } catch {
-                await interaction.editReply({
-                    content: 'Возникла ошибка при получении данных либо данных вашего сервера нет.',
-                    ephemeral: true,
-                });
+                await interaction.editReply(
+                    '**Данные сервера в формате JSON:**' + '```json\n' + JSON.stringify(data, null, ' ') + '\n```'
+                );
+            } catch (err) {
+                console.log(err);
+                await interaction.editReply('Возникла ошибка при получении данных либо данных вашего сервера нет.');
             }
         } else {
             accessDenied(interaction);
